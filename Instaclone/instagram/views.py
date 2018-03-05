d6bf2ab4c8568d8a49e115741e433e0ff3b61679
June Muoti <june.muoti@gmail.com>
2018-03-05 15:28:57 +0300

    define view functions for profile,index and search results

---
+++ Instaclone/instagram/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Image,Profile
+
+
+
+@login_required(login_url='/accounts/login/')
+
+def index(request):
+    images=Image.my_image()
+    return render(request,'index.html',{"images":images})
+def profile(request):
+    profiles=Profile.my_profile()
+    return render(request,'profile.html',{"profiles":profiles})
+
+
+def search_results(request):
+    if 'user' in request.GET and request.GET["user"]:
+        queryList = request.GET.get("user")
+        searched_users = Image.search_by_user(queryList)
+        message = f"{queryList}"
+
+        return render(request, 'search.html',{"message":message,"users": searched_users})
+
+    else:
+        message = "You haven't searched for any user"
+        return render(request, 'search.html',{"message":message})
+
+# Create your views here.
