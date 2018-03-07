6c846ece6194d719b1d5262020eead11cbb06d30
June Muoti <june.muoti@gmail.com>
2018-03-07 11:05:57 +0300

    register  likes model

--- Instaclone/instagram/admin.py
+++ Instaclone/instagram/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Image,Profile
+from .models import Image,Profile,Likes
 
 admin.site.register(Image)
 admin.site.register(Profile)
+admin.site.register(Likes)
+
+
 
 # Register your models here.
