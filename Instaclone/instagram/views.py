c03a98d7620f2db9d2ab5b12f149d0f5dc1c3922
June Muoti <june.muoti@gmail.com>
2018-03-07 11:11:51 +0300

    add comments  form

--- Instaclone/instagram/views.py
+++ Instaclone/instagram/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Image,Profile
+from .forms import NewImageForm,CommentsForm
+from django.http import JsonResponse
 
 
 
@@ -9,12 +11,19 @@ from .models import Image,Profile
 
 def index(request):
     images=Image.my_image()
-    return render(request,'index.html',{"images":images})
+    form = CommentsForm()
+    return render(request,'index.html',{"images":images},{"commentForm": form})
+def comments(request):
+    comment = request.POST.get('your_comment')
+
+    return JsonResponse()
+
 
 @login_required(login_url='/accounts/login/')
 def profile(request,user_id):
     profiles= Profile.objects.get(id = user_id)
-    return render(request,'profile.html',{"profiles":profiles})
+    images=Image.objects.all()
+    return render(request,'profile.html',{"profiles":profiles},{"images":images})
 
 
 def search_results(request):
@@ -28,5 +37,18 @@ def search_results(request):
     else:
         message = "You haven't searched for any user"
         return render(request, 'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def new_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.editor = current_user
+            image.save()
+    else:
+        form = NewImageForm()
+    return render(request, 'new_image.html', {"form": form})
 
 # Create your views here.
