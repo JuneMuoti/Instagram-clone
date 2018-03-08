d4845d41529a86e1a8255addbd075ae95644d68c
June Muoti <june.muoti@gmail.com>
2018-03-08 11:18:18 +0300

    configure app urls

---
+++ instagram/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'^profile/(\d+)',views.profile,name = 'profile'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^ajax/comments/$', views.comments, name='comment'),
+    url(r'^comment/(?P<image_id>[0-9]+)/$', views.add_comment, name='add_comment'),
+
+
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
